90062684c4518ec7901fc13a8cd3c8698fcd88de
Hansika Sachdeva <52819652+HansikaSachdeva@users.noreply.github.com>
2021-07-05 23:26:25 +0530

    Update Stock Prediction.py

--- Stock Prediction.py
+++ Stock Prediction.py
@@ -17,7 +17,7 @@ today = date.today().strftime("%Y-%m-%d")
 st.title('Stock Forecast App')
 
 #reading csv file with ticker symbols into a data frame
-df = pd.read_csv(r'C:\Users\hansi\Desktop\Projects\stock_prediction_app\Yahoo-Finance-Ticker-Symbols.csv')
+df = pd.read_csv(r'path\to\csv')
 stocks = df
 selected_stock = st.selectbox("Select stock for prediction", stocks)
 
